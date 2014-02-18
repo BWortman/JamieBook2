7f19985462c3282f4d32babeb571c4fa9eb2a5b8
Brian Wortman <brian.wortman@outlook.com>
2014-02-17 21:59:39 -0500

    roughing-in legacy wpf client

---
+++ src/WebApi2Book.Web.Legacy.Api/App_Code/TeamTaskService.cs
@@ -0,0 +1,42 @@
+﻿// TeamTaskService.cs
+// Copyright Jamie Kurtz, Brian Wortman 2014.
+
+using System.Collections.Generic;
+using System.Web.Services;
+using WebApi2Book.Web.Api.Models;
+
+/// <summary>
+///     Summary description for TeamTaskService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class TeamTaskService : WebService
+{
+    public TeamTaskService()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public Category[] GetCategories()
+    {
+        return new[]
+        {
+            new Category {Name = "cat1", Description = "category 1"},
+            new Category {Name = "cat2", Description = "category 2"}
+        };
+    }
+
+    [WebMethod]
+    public Status[] GetStatuses()
+    {
+        return new[]
+        {
+            new Status() {Name = "status1"},
+            new Status() {Name = "status2"}
+        };
+    }
+}
---
+++ src/WebApi2Book.Windows.Legacy.Client/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿// MainWindow.xaml.cs
+// Copyright Jamie Kurtz, Brian Wortman 2014.
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.Practices.Prism.Commands;
+using WebApi2Book.Windows.Legacy.Client.TaskServiceReference;
+
+namespace WebApi2Book.Windows.Legacy.Client
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        private readonly TeamTaskServiceSoapClient _taskServiceSoapClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            GetCategoriesCommand = new DelegateCommand(GetCategories);
+            Categories = new ObservableCollection<Category>();
+
+            _taskServiceSoapClient = new TeamTaskServiceSoapClient();
+            _taskServiceSoapClient.Open();
+        }
+
+        public ObservableCollection<Category> Categories { get; private set; }
+
+        public DelegateCommand GetCategoriesCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public void GetCategories()
+        {
+            Categories.Clear();
+            var result = _taskServiceSoapClient.GetCategories();
+            result.ToList().ForEach(x => Categories.Add(x));
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
