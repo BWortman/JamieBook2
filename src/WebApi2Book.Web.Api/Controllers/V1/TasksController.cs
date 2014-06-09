05e185050061e2436f814d2de4d53c60c353bdc9
Jamie Kurtz <jkurtz2530@gmail.com>
2014-06-08 22:04:47 -0400

    small change to use of dependencyblock; removing debug code

--- src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
+++ src/WebApi2Book.Web.Api/Controllers/V1/TasksController.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using WebApi2Book.Common;
+using WebApi2Book.Data;
+using WebApi2Book.Web.Api.InquiryProcessing;
 using WebApi2Book.Web.Api.MaintenanceProcessing;
 using WebApi2Book.Web.Api.Models;
 using WebApi2Book.Web.Common;
@@ -19,28 +21,35 @@ namespace WebApi2Book.Web.Api.Controllers.V1
     [EnableCors("http://localhost:52976", "*", "*")]
     public class TasksController : ApiController
     {
-        private readonly ITasksControllerDependencyBlock _tasksControllerDependencyBlock;
+        private readonly IAddTaskMaintenanceProcessor _addTaskMaintenanceProcessor;
+        private readonly IAllTasksInquiryProcessor _allTasksInquiryProcessor;
+        private readonly IDeleteTaskQueryProcessor _deleteTaskQueryProcessor;
+        private readonly IPagedDataRequestFactory _pagedDataRequestFactory;
+        private readonly ITaskByIdInquiryProcessor _taskByIdInquiryProcessor;
+        private readonly IUpdateTaskMaintenanceProcessor _updateTaskMaintenanceProcessor;
 
         public TasksController(ITasksControllerDependencyBlock tasksControllerDependencyBlock)
         {
-            _tasksControllerDependencyBlock = tasksControllerDependencyBlock;
+            _addTaskMaintenanceProcessor = tasksControllerDependencyBlock.AddTaskMaintenanceProcessor;
+            _allTasksInquiryProcessor = tasksControllerDependencyBlock.AllTasksInquiryProcessor;
+            _deleteTaskQueryProcessor = tasksControllerDependencyBlock.DeleteTaskQueryProcessor;
+            _pagedDataRequestFactory = tasksControllerDependencyBlock.PagedDataRequestFactory;
+            _taskByIdInquiryProcessor = tasksControllerDependencyBlock.TaskByIdInquiryProcessor;
+            _updateTaskMaintenanceProcessor = tasksControllerDependencyBlock.UpdateTaskMaintenanceProcessor;
         }
 
         [Route("", Name = "GetTasksRoute")]
         public PagedDataInquiryResponse<Task> GetTasks(HttpRequestMessage requestMessage)
         {
-            var request = _tasksControllerDependencyBlock.PagedDataRequestFactory.Create(requestMessage.RequestUri);
-
-            var cookies = Request.Headers.GetCookies();
-
-            var tasks = _tasksControllerDependencyBlock.AllTasksInquiryProcessor.GetTasks(request);
+            var request = _pagedDataRequestFactory.Create(requestMessage.RequestUri);
+            var tasks = _allTasksInquiryProcessor.GetTasks(request);
             return tasks;
         }
 
         [Route("{id:long}", Name = "GetTaskRoute")]
         public Task GetTask(long id)
         {
-            var task = _tasksControllerDependencyBlock.TaskByIdInquiryProcessor.GetTask(id);
+            var task = _taskByIdInquiryProcessor.GetTask(id);
             return task;
         }
 
@@ -50,7 +59,7 @@ namespace WebApi2Book.Web.Api.Controllers.V1
         [Authorize(Roles = Constants.RoleNames.Manager)]
         public IHttpActionResult AddTask(HttpRequestMessage requestMessage, NewTask newTask)
         {
-            var task = _tasksControllerDependencyBlock.AddTaskMaintenanceProcessor.AddTask(newTask);
+            var task = _addTaskMaintenanceProcessor.AddTask(newTask);
 
             var result = new TaskCreatedActionResult(requestMessage, task);
 
@@ -64,7 +73,7 @@ namespace WebApi2Book.Web.Api.Controllers.V1
         [Authorize(Roles = Constants.RoleNames.SeniorWorker)]
         public Task UpdateTask(long id, [FromBody] object updatedTask)
         {
-            var task = _tasksControllerDependencyBlock.UpdateTaskMaintenanceProcessor.UpdateTask(id, updatedTask);
+            var task = _updateTaskMaintenanceProcessor.UpdateTask(id, updatedTask);
             return task;
         }
 
@@ -73,7 +82,7 @@ namespace WebApi2Book.Web.Api.Controllers.V1
         [Authorize(Roles = Constants.RoleNames.Manager)]
         public IHttpActionResult DeleteTask(long id)
         {
-            _tasksControllerDependencyBlock.DeleteTaskQueryProcessor.DeleteTask(id);
+            _deleteTaskQueryProcessor.DeleteTask(id);
             return Ok();
         }
     }
